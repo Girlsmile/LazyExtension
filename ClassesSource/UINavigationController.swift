bb6cf3b030834445e6de488298df2f95dae92b09
GZP <1965143467@qq.com>
2020-05-06 10:18:03 +0800

    [add] navigatenavigate

--- ClassesSource/UINavigationController.swift
+++ ClassesSource/UINavigationController.swift
@@ -1,20 +1,10 @@
 //
-//  BaseNavigateController.swift
+//  UINavigateController.swift
 //  CertificateCamera
 //
-//  Created by 古智鹏 on 2020/2/18.
+//  Created by 古智鹏 on 2020/5/6.
 //  Copyright © 2020 古智鹏. All rights reserved.
 //
-
-import Foundation
-public class BaseNavigateController: UINavigationController {
-    
-    override public func pushViewController(_ viewController: UIViewController, animated: Bool) {
-        self.navigationBar.topItem?.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
-        super.pushViewController(viewController, animated: animated)
-    }
-}
-
 
 public protocol VCWithBackButtonHandler {
     func shouldPopOnBackButton() -> Bool
