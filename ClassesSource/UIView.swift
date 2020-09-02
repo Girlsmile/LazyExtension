3d8f7020f68d8797ee635bfc449b521e84faa419
GZP <1965143467@qq.com>
2020-09-02 10:38:56 +0800

    [add] view

--- ClassesSource/UIView.swift
+++ ClassesSource/UIView.swift
@@ -7,7 +7,14 @@
 //
 
 import Foundation
+import UIKit
+import SnapKit
+
 public extension UIView {
+    
+    enum Edges {
+        case leftTop, rightTop, leftBottom, rightBottom
+    }
     
     func roundCorners(corners: UIRectCorner, radius: CGFloat) {
         let path = UIBezierPath(roundedRect: bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
@@ -100,5 +107,72 @@ public extension UIView {
         return UIImage()
     }
     
+    func dotInsert(color: UIColor = UIColor(hex:0xFF4630), edage: Edges = .rightTop, radius: CGFloat = 5.0, inset: CGFloat = 2.0) {
+        
+        subviews.forEach({
+            if $0.tag == 998 {
+                $0.removeFromSuperview()
+            }
+        })
+        
+        let view = UIView()
+        view.backgroundColor = color
+        view.tag = 998
+        view.layer.cornerRadius = radius
+        view.layer.masksToBounds = true
+        
+        self.addSubview(view)
+        
+        switch  edage {
+            case .leftBottom:
+                view.snp.remakeConstraints { (make) in
+                    make.left.bottom.equalToSuperview().inset(inset)
+                    make.size.equalTo(radius*2)
+                }
+            case .leftTop:
+                view.snp.remakeConstraints { (make) in
+                    make.left.top.equalToSuperview().inset(inset)
+                    make.size.equalTo(radius*2)
+                }
+            case .rightBottom:
+                view.snp.remakeConstraints { (make) in
+                    make.right.bottom.equalToSuperview().inset(inset)
+                    make.size.equalTo(radius*2)
+                }
+            case .rightTop:
+                view.snp.remakeConstraints { (make) in
+                    make.right.top.equalToSuperview().inset(inset)
+                    make.size.equalTo(radius*2)
+                }
+        }
+        
+        view.isHidden = false
+    }
     
+    func dotDismiss() {
+        subviews.forEach({
+            if $0.tag == 998 {
+                $0.isHidden = true
+                $0.removeFromSuperview()
+            }
+        })
+    }
+    
+    
+}
+
+
+public extension UIColor {
+    convenience init(red: Int, green: Int, blue: Int, a: CGFloat = 1.0) {
+        assert(red >= 0 && red <= 255, "Invalid red component")
+        assert(green >= 0 && green <= 255, "Invalid green component")
+        assert(blue >= 0 && blue <= 255, "Invalid blue component")
+        
+        self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: a)
+    }
+    
+    convenience init(hex: Int, alpha: CGFloat = 1.0) {
+        self.init(red:(hex >> 16) & 0xff, green:(hex >> 8) & 0xff, blue:hex & 0xff, a: alpha)
+    }
+
 }
